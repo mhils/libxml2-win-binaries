0c22a7c55f126ebe6aa551ae2ef50bc7f82f71d4
Maximilian Hils <git@maximilianhils.com>
2016-06-27 18:43:42 -0700

    adjust to lxml expectations

--- build.ps1
+++ build.ps1
@@ -11,7 +11,12 @@ cd .\libiconv\MSVC14
 msbuild libiconv.sln /p:Configuration=Release 
 # /p:Platform=Win32
 # msbuild libiconv.sln /p:Configuration=Release /p:Platform=x64
-$iconvLib = Join-Path (pwd) Release
+$iconvLib = Join-Path (pwd) libiconv_static\Release
+
+# lxml expects this to be called iconv, not libiconv
+Dir $iconvLib\iconv* | Remove-Item -Recurse
+Dir $iconvLib\libiconv* | Rename-Item -NewName { $_.Name -replace "libiconv","iconv" }
+
 $iconvInc = Join-Path (pwd) ..\source\include
 cd ..\..
 
@@ -25,8 +30,6 @@ cd ..\..
 cd .\libxslt\win32
 cscript configure.js lib="$iconvLib;$xmlLib" include="$iconvInc;$xmlInc" vcmanifest=yes
 nmake
-$xsltLib = Join-Path (pwd) bin.msvc
-$xsltInc = Join-Path (pwd) ..\libxslt
 cd ..\..
 
 
@@ -37,15 +40,18 @@ Function BundleRelease($name, $lib, $inc)
 
     New-Item -ItemType Directory .\dist\$name\lib
     Copy-Item -Recurse $lib .\dist\$name\lib
+    Get-ChildItem -File -Recurse .\dist\$name\lib | Where{$_.Name -NotMatch ".(lib|exp)$" } | Remove-Item
 
     New-Item -ItemType Directory .\dist\$name\include
     Copy-Item -Recurse $inc .\dist\$name\include
-    Compress-Archive  .\dist\$name .\dist\$name.zip
+    Get-ChildItem -File -Recurse .\dist\$name\include | Where{$_.Name -NotMatch ".h$" } | Remove-Item
+
+    Write-Zip  .\dist\$name .\dist\$name.zip
     Remove-Item -Recurse -Path .\dist\$name
 }
 
 if (Test-Path .\dist) { Remove-Item .\dist -Recurse }
 New-Item -ItemType Directory .\dist
-BundleRelease "iconv-1.14.win32" (dir $iconvLib\*) (dir $iconvInc\*.h)
-BundleRelease "libxml2-2.9.4.win32" (dir $xmlLib\*.lib,$xmlLib\*.exp) (dir $xmlInc\libxml\*.h)
-BundleRelease "libxslt-1.1.27.win32" (dir $xsltLib\*.lib,$xsltLib\*.exp) (dir $xsltInc\*.h)
+BundleRelease "iconv-1.14.win32" (dir $iconvLib\*) (dir $iconvInc\*)
+BundleRelease "libxml2-2.9.4.win32" (dir $xmlLib\*) (Get-Item $xmlInc\libxml)
+BundleRelease "libxslt-1.1.27.win32" (dir .\libxslt\win32\bin.msvc\*) (Get-Item .\libxslt\libxslt,.\libxslt\libexslt)
